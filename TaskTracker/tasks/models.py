f2ed25e13d22def7ed4431b2227c69d88c04d693
Şerife Nazlı Ünay <unay.nazli42@gmail.com>
2025-07-28 18:20:12 +0300

    Create tasks app and add Task model

---
+++ TaskTracker/tasks/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Task(models.Model):
+    status=models.CharField(max_length=200) # The status of the task (todo, in-progress, done)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
