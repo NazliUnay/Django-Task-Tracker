5c0beb67dc58a06272b8762b96430812bee9ac24
Şerife Nazlı Ünay <unay.nazli42@gmail.com>
2025-07-28 19:13:13 +0300

    Add task update and delete functionality with corresponding views, URLs, and templates

--- TaskTracker/tasks/urls.py
+++ TaskTracker/tasks/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('ekle/', views.task_create, name='task_create'),
     path('completed/', views.completed_tasks, name='completed_tasks'),
+    path('task/<int:pk>/edit/', views.task_update, name='task_update'),
+    path('task/<int:pk>/delete/', views.task_delete, name='task_delete'),
 ]
